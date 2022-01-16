553e462912951fa08f3f3cc9289e6b2a89979713
Karan Tandon <karantandon1991@gmail.com>
2022-01-16 15:46:24 +0530

    api to load sample data

---
+++ src/api/services/load_data.py
@@ -0,0 +1,33 @@
+import json
+import os
+import logging
+
+from bson import ObjectId
+
+from api.repository import Repository
+from api.schemas.game import GameIn
+from api.schemas.image import ImageOut
+
+async def load_json_data(db: Repository):
+    curr_dir = os.path.dirname(__file__)
+    fixtures_dir = os.path.join(curr_dir, "..", "fixtures")
+    sample_json_data_file = os.path.join(fixtures_dir, "UITest.json")
+    
+    logging.info(f"Reading Json file from {sample_json_data_file}")
+    
+    with open(sample_json_data_file) as json_file:
+        data = json.load(json_file)
+        logging.info("Read json file.")
+        game_listings = data["listings"]
+        games = []
+        for game_listing in game_listings:
+            images = []
+            game = GameIn(**game_listing)
+            for img in game.images:
+                images.append(ImageOut(**img.dict(), id=ObjectId()))
+            game.images=images
+            games.append(game.dict())
+        logging.info("Loading sample data...")
+        await db.games.insert_many(games)  
+        logging.info("Loaded sample data.")
+
--- src/app.py
+++ src/app.py
@@ -1,11 +1,29 @@
-from fastapi import FastAPI
 import uvicorn
+
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+
 from api.config import Config
-from api.routes import games
+from api.routes import auth, games, users
 from api.repository import db
+from api.services import load_data as load_data_service
+
 app = FastAPI(title="Unity Challenge")
 
+app.include_router(auth.router)
 app.include_router(games.router, prefix='/api/games')
+app.include_router(users.router, prefix='/api/users')
+
+
+@app.get("/")
+async def docs_redirect():
+    return RedirectResponse(url='/docs')
+
+
+@app.get("/load")
+async def load_sample_data():
+    await load_data_service.load_json_data(db.db)
+    return {"message": "Sample data loaded successfully."}
 
 
 @app.on_event("startup")
@@ -17,6 +35,7 @@ async def startup():
 @app.on_event("shutdown")
 async def shutdown():
     await db.close()
+
 
 if __name__ == "__main__":
     uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
