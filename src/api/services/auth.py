48c5c36c716f5d5bcfb44e14dadbeeef445bd692
Karan Tandon <karantandon1991@gmail.com>
2022-01-17 06:25:25 +0530

    fixing login route

--- src/api/services/auth.py
+++ src/api/services/auth.py
@@ -1,12 +1,11 @@
 from fastapi import HTTPException, status
-from fastapi.security import OAuth2PasswordRequestForm
 
 from api import utils, oauth2
 from api.repository import Repository
 from api.schemas.token import Token
+from api.schemas.user import UserIn
 
-
-async def login(user_credentials: OAuth2PasswordRequestForm, db: Repository) -> Token:
+async def login(user_credentials: UserIn, db: Repository) -> Token:
     user = await db.users.find_one({"email": user_credentials.username})
     if not user:
         raise HTTPException(
